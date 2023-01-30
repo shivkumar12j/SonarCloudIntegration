7e11837356f4a05ece9696491ae0c929c12f2710
Shiv Kumar <shiv.kumar@itcube.net>
2023-01-30 13:35:00 +0530

    added new method CalculatC()

--- Class1.cs
+++ Class1.cs
@@ -14,13 +14,18 @@ public class Class1
 
     public int CalculatB()
     {
-        //commented code
         int count = 1;
         for (int i = 0; i < 10; i++)
         {
             count++;
         }
         count += 50;
+
+        return count;
+    }
+    public int CalculatC()
+    {
+        int count = 100;
 
         return count;
     }
