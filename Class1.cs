af32ae255efd379b46742ee82cd67ff222842d61
Shiv Kumar <shiv.kumar@itcube.net>
2023-01-30 17:50:35 +0530

    added code

--- Class1.cs
+++ Class1.cs
@@ -8,20 +8,36 @@ public class Class1
         {
             count++;
         }
-
         return count;
     }
 
     public int CalculatB()
     {
-        //commented code
         int count = 1;
         for (int i = 0; i < 10; i++)
         {
             count++;
         }
         count += 50;
+        return count;
+    }
 
+    public int CalculatC()
+    {
+        int count = 0;
+        string code = "5";
+        switch (count)
+        {
+            case "1":
+                count = 1;
+                break;
+            case "2":
+                count = 2;
+                break;
+            case "4":
+                count = 4;
+                break;
+        }
         return count;
     }
 
